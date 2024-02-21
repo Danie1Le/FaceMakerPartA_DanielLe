c4a1f68ba06a49f3db51bae304eb1f2efcf29c22
Danie1Le <danielle8262005@gmail.com>
2024-02-21 15:55:57 -0800

    Initial Commit

--- app/src/main/java/com/example/facemakerparta_danielle/Face.java
+++ app/src/main/java/com/example/facemakerparta_danielle/Face.java
@@ -1,34 +1,43 @@
 // @author Daniel Le
 package com.example.facemakerparta_danielle;
 
+import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+import android.widget.ArrayAdapter;
 
+
+import java.util.ArrayList;
 import java.util.Random;
 
-public class Face {
+public class Face extends SurfaceView {
     private int skinColor;
     private int eyeColor;
     private int hairColor;
     private int hairStyle;
 
-    public Face() {
-        randomize(); // call the randomize method
+    public Face(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setWillNotDraw(false);
+        //randomizes the variables
+        randomize();
+
+        setBackgroundColor(Color.WHITE);
     }
 
     public void randomize() {
         Random rand = new Random(); // makes a random generator to be used on the variables
-
         // randomly assign the variables with values 0 - 255
         skinColor = rand.nextInt(256);
-        eyeColor  = rand.nextInt(256);
+        eyeColor = rand.nextInt(256);
         hairColor = rand.nextInt(256);
 
         // randomize with 0 - 3
         hairStyle = rand.nextInt(4);
-    }
-
-    public void onDraw(Canvas Canvas) {
-        // suppose to be empty for now
     }
 
     // setter and getter methods that would be used with the onDraw method
@@ -45,7 +54,7 @@ public class Face {
     }
 
     public int getEyeColor() {
-        return  this.eyeColor;
+        return this.eyeColor;
     }
 
     public void setHairColor(int hairColor) {
@@ -62,5 +71,64 @@ public class Face {
 
     public int getHairStyle() {
         return this.hairStyle;
+    }
+
+    public void onDraw(Canvas canvas) {
+
+        // Get midpoints for horizontal and vertical
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() / 2;
+
+        // Draw face background
+        Paint skinColor = new Paint();
+        int skinColorValue = Color.argb(255, getSkinColor(), getSkinColor(), getSkinColor()); // set RGB values based on random gen
+        skinColor.setColor(skinColorValue); // set color to that
+        canvas.drawCircle(centerX, centerY, 500.0f, skinColor);
+
+        // Draw the eyes of the face
+        Paint EyeColor = new Paint();
+        int EyeColorValue = Color.argb(255, getEyeColor(), getEyeColor(), getEyeColor()); // set RGB values based on random gen
+        EyeColor.setColor(EyeColorValue); // set the color to that
+        canvas.drawCircle(centerX - 200, centerY - 80, 50.0f, EyeColor);
+        canvas.drawCircle(centerX + 200, centerY - 80, 50.0f, EyeColor);
+
+        // Draw hair based on hairstyle
+        Paint hairPaint = new Paint();
+        int HairColorValue = Color.argb(255, getHairColor(), getHairColor(), getHairColor()); // set RGB values based on random gen
+        hairPaint.setColor(HairColorValue); // set color to that
+        if (hairStyle == 0) {
+            // Draw Buzz hairstyle
+            canvas.drawRect(centerX - 500, centerY - 500, centerX + 500, centerY - 300, hairPaint); // top portion
+            canvas.drawRect(centerX - 500, centerY - 500, centerX - 400, centerY - 100, hairPaint); // left side
+            canvas.drawRect(centerX + 400, centerY - 500, centerX + 500, centerY - 100, hairPaint); // right side
+        }
+        else if (hairStyle == 1) {
+            // this will be the taper
+            canvas.drawRect(centerX - 525, centerY - 200, centerX - 400, centerY - 100, hairPaint); // left side
+            canvas.drawRect(centerX - 550, centerY - 400, centerX - 350, centerY - 200, hairPaint); // left side
+            canvas.drawRect(centerX + 400, centerY - 200, centerX + 525, centerY - 100, hairPaint); // right side
+            canvas.drawRect(centerX + 375, centerY - 400, centerX + 550, centerY - 200, hairPaint); // right side
+            canvas.drawRect(centerX - 500, centerY - 550, centerX + 500, centerY - 300, hairPaint); // top portion
+        } else if (hairStyle == 2) {                // Afro
+            canvas.drawRect(centerX - 500, centerY - 500, centerX - 400, centerY - 100, hairPaint); // left side
+            canvas.drawRect(centerX + 400, centerY - 500, centerX + 500, centerY - 100, hairPaint); // right side
+            for (int i = 0; i < 5; i++) {
+                canvas.drawCircle(centerX - 400.0f + (200 * i), centerY - 450.0f, 150, hairPaint); // for loop to create afro effect on top of head
+            }
+        } else if (hairStyle == 3) {
+            // fade
+            // for loop to make it draw over and over to have a certain pattern
+            for (int i = 0; i < 6; i++) {
+                canvas.drawRect(centerX - 500 - (5 * i), centerY - 200 - (30 * i), centerX - 400 + (10 * i), centerY - 100 - (30 * i), hairPaint); // left side
+                canvas.drawRect(centerX + 400 - (10 * i), centerY - 200 - (30 * i), centerX + 500 + (5 * i), centerY - 100 - (30 * i), hairPaint); // right side
+                canvas.drawCircle(centerX - 375.0f + (150 * i), centerY - 450.0f, 170, hairPaint); // top part of hair
+            }
+        }
+        else {
+            // default to buzz
+            canvas.drawRect(centerX - 500, centerY - 500, centerX + 500, centerY - 300, hairPaint); // top portion
+            canvas.drawRect(centerX - 500, centerY - 500, centerX - 400, centerY - 100, hairPaint); // left side
+            canvas.drawRect(centerX + 400, centerY - 500, centerX + 500, centerY - 100, hairPaint); // right side
+        }
     }
 }
--- app/src/main/java/com/example/facemakerparta_danielle/MainActivity.java
+++ app/src/main/java/com/example/facemakerparta_danielle/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.facemakerparta_danielle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -15,92 +19,96 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
         // initializes SeekBars and TextViews
-        SeekBar seekBarRed;
-        SeekBar seekBarBlue;
-        SeekBar seekBarGreen;
-        TextView tvOne;
-        TextView tvTwo;
-        TextView tvThree;
-
-            //seekbar for Red
-            seekBarRed = findViewById(R.id.RedVal);
-            //seekbar for Green;
-            seekBarGreen = findViewById(R.id.GreenVal);
-            //seekbar for Blue;
-            seekBarBlue = findViewById(R.id.BlueVal);
-
-            //references the textview
-            tvOne = findViewById(R.id.RedNum);
-            tvTwo = findViewById(R.id.GreenNum);
-            tvThree = findViewById(R.id.BlueNum);
+        SeekBar seekBarRed = findViewById(R.id.RedVal);
+        SeekBar seekBarBlue = findViewById(R.id.BlueVal);
+        SeekBar seekBarGreen = findViewById(R.id.GreenVal);
+        TextView tvOne = findViewById(R.id.RedNum);
+        TextView tvTwo = findViewById(R.id.GreenNum);
+        TextView tvThree = findViewById(R.id.BlueNum);
+        RadioButton HairButton = findViewById(R.id.hairRadioButton);
+        RadioButton EyesButton = findViewById(R.id.eyesRadioButton);
+        RadioButton SkinButton = findViewById(R.id.skinRadioButton);
+        Button randomButton = findViewById(R.id.RandomButton);
+        Face faceView = findViewById(R.id.FaceDisplay);
 
 
-            // setup The seekBar progress for red
-            seekBarRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int Progress, boolean b) {
-                    tvOne.setVisibility(View.VISIBLE);
+        // setup The seekBar progress for red
+        seekBarRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int Progress, boolean b) {
+                tvOne.setVisibility(View.VISIBLE);
+                //sets the text based on progress value
+                tvOne.setText("Red: " + Progress + "");
 
-                    //sets the text based on progress value
-                    tvOne.setText("Red: " + Progress + "");
-                }
+                faceView.setHairColor(Color.rgb(Progress, seekBarGreen.getProgress(), seekBarBlue.getProgress()));
+                faceView.invalidate();
 
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
+            }
 
-                }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
+            }
 
-                }
-            });
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
 
-        // setup The seekBar progress for green
-        seekBarBlue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int Progress, boolean b) {
-                    tvTwo.setVisibility(View.VISIBLE);
-                    tvTwo.setText("Green: " + Progress + "");
-                }
+            }
+        });
 
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-
-                }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-
-                }
-            });
-
-        // setup The seekBar progress for blue
+        // setup The seekBar progress for Green
         seekBarGreen.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int Progress, boolean b) {
-                    tvThree.setVisibility(View.VISIBLE);
-                    tvThree.setText("Blue: " + Progress + "");
-                }
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int Progress, boolean b) {
+                tvThree.setVisibility(View.VISIBLE);
+                tvThree.setText("Blue: " + Progress + "");
 
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
+                faceView.setHairColor(Color.rgb(seekBarRed.getProgress(), Progress, seekBarBlue.getProgress()));
+                faceView.invalidate();
+            }
 
-                }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
+            }
 
-                }
-            });
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        // setup The seekBar progress for Blue
+        seekBarBlue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int Progress, boolean b) {
+                tvTwo.setVisibility(View.VISIBLE);
+                tvTwo.setText("Green: " + Progress + "");
+
+                faceView.setHairColor(Color.rgb(seekBarRed.getProgress(), seekBarGreen.getProgress(), Progress));
+                faceView.invalidate();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
         // Set up the Spinner for all the Hairstyles
         Spinner spinner = findViewById(R.id.HairStyles);
@@ -109,25 +117,59 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 String item = adapterView.getItemAtPosition(i).toString();
                 Toast.makeText(MainActivity.this, "Selected Item: " + item, Toast.LENGTH_SHORT).show();
+                faceView.setHairStyle(i);
+                faceView.invalidate();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
-
         });
 
         // Make an Arraylist to hold all of the hairstyle options for the spinner
         ArrayList<String> HairTypes = new ArrayList<>();
         HairTypes.add("Buzz");
         HairTypes.add("Taper");
-        HairTypes.add("Cornrows");
+        HairTypes.add("Afro");
         HairTypes.add("Fade");
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, HairTypes);
         adapter.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
         spinner.setAdapter(adapter);
+
+        // Set up the RadioGroup for hair, eyes, and skin RadioButtons
+        RadioGroup radioGroup = findViewById(R.id.RadioGroupOptions);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                // Check which RadioButton is selected and update SeekBars accordingly
+                if (checkedId == R.id.hairRadioButton) {
+                    updateSeekBars(faceView.getHairColor());
+                }
+                else if (checkedId == R.id.eyesRadioButton) {
+                    updateSeekBars(faceView.getEyeColor());
+                }
+                else if (checkedId == R.id.skinRadioButton) {
+                    updateSeekBars(faceView.getSkinColor());
+                }
+            }
+        });
+
+        setUpSeekBarListeners();
+    }
+
+    private void setUpSeekBarListeners() {
+
+    }
+
+    // Method to update SeekBars based on a color value
+    private void updateSeekBars(int color) {
+        SeekBar seekBarRed = findViewById(R.id.RedVal);
+        SeekBar seekBarGreen = findViewById(R.id.GreenVal);
+        SeekBar seekBarBlue = findViewById(R.id.BlueVal);
+
+        seekBarRed.setProgress(Color.red(color));
+        seekBarGreen.setProgress(Color.green(color));
+        seekBarBlue.setProgress(Color.blue(color));
     }
 }
-
-
